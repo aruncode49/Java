66fa9d4e56d6e041033774591d35dfb5e32f6aef
Arun Kumar <aruncode49@gmail.com>
2023-10-15 01:13:33 +0530

    Chapter-1 Variable and Datatypes Output in java

---
+++ Chapter -1 Variables and Data Types/Output.java
@@ -0,0 +1,10 @@
+public class Output {
+    public static void main(String args[]) {
+        // print Hello World
+        System.out.print("Hello\n");
+
+        // print Hello Word in first line and print Hello Arun in next line
+        System.out.println("Hello World");
+        System.out.println("Hello Arun");
+    }
+}
