8151a328cce77377b909822357ef62a0b8659ecc
Arun Kumar <aruncode49@gmail.com>
2023-10-16 00:50:07 +0530

    Optimize prime number

--- Chapter -3 Loops/Prime_number.java
+++ Chapter -3 Loops/Prime_number.java
@@ -1,30 +1,32 @@
 // check wheather a given number is prime or not
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Prime_number {
     public static void main(String[] args) {
-        int n, i = 0;
+        int n;
         // input number
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your number: ");
         n = sc.nextInt();
 
-        if (n < 2) {
+        boolean isPrime = true;
+
+        if (n == 1) {
             System.out.println("Not Prime");
-        }
-
-        // check prime or not
-        for (i = 2; i < n; i++) {
-            if (n % i == 0) {
-                System.out.println("Not Prime Number");
-                break;
-            }
-        }
-
-        // check how we comes out from the loop, from break or not
-        if (i == n) {
+        } else if (n == 2) {
             System.out.println("Prime Number");
+        } else {
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) {
+                    isPrime = false;
+                }
+            }
+            if (isPrime) {
+                System.out.println("Prime Number");
+            } else {
+                System.out.println("Not Prime");
+            }
         }
 
     }
