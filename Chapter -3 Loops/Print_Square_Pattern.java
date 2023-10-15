99edef99bda87a434916ca5a65805a9f582e7174
Arun Kumar <aruncode49@gmail.com>
2023-10-15 23:44:49 +0530

    print square pattern

---
+++ Chapter -3 Loops/Print_Square_Pattern.java
@@ -0,0 +1,15 @@
+// ****
+// ****
+// ****
+// ****
+
+public class Print_Square_Pattern {
+    public static void main(String[] args) {
+        int n = 4;
+        while (n > 0) {
+            System.out.println("****");
+            n--;
+        }
+    }
+
+}
